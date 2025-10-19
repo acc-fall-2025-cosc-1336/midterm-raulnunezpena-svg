f7fbbdb193045cf08ec296eeb19884fb2d099c6e
raulnunezpena-svg <raul.nunezpena@g.austincc.edu>
2025-10-19 02:05:06 +0000

    degrees f and c

--- src/question_b/main.py
+++ src/question_b/main.py
@@ -1,1 +1,12 @@
-#add import
+from question_b import get_fahrenheit
+
+def main():
+    print("Celsius\tFahrenheit")
+    print("-------------------")
+    
+    for celsius in range(21):  # 0 through 20 inclusive
+        fahrenheit = get_fahrenheit(celsius)
+        print(f"{celsius}\t{fahrenheit:.1f}")
+
+if __name__ == "__main__":
+    main()
--- src/question_b/question_b.py
+++ src/question_b/question_b.py
@@ -1,1 +1,2 @@
-#write functions here, don't add input('') statements here!
+def get_fahrenheit(celsius):
+    return (9/5) * celsius + 32
